1c4622601271e9329c408e96c0b6958cac149d65
Nathanielx3rd <81175228+nathaniel123x3rd@users.noreply.github.com>
2024-04-11 12:54:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -92,4 +92,26 @@ client.on('interactionCreate', (interaction) => {
   }
 });
 
+const commands = [{
+    name: 'support',
+    description: 'Shows the support server!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Registering slash commands...');
+    
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+    console.log('Slash commands were registered successfully');
+  } catch (error) {
+    console.log(`There was an error: ${error}`);
+  }
+})();
+
 client.login(process.env.TOKEN);
